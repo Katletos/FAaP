0c432e4aeed946d17ca8a3ed76d59978c3f13cfc
Mikita <40485432+Katletos@users.noreply.github.com>
2021-10-21 19:24:06 +0300

    Add files via upload

--- SelectionSort.dpr
+++ SelectionSort.dpr
@@ -1,7 +1,6 @@
 ﻿program SelectionSort;
 
 {$APPTYPE CONSOLE}
-
 {$R *.res}
 
 uses
@@ -11,36 +10,38 @@ const
   N = 10;
 
 type
-  mas = array[1..N] of integer;
+  mas = array [1 .. N] of integer;
 
 var
   A: mas;
   i, j, min, tmp: integer;
 
 begin
-  writeln ('Enter 10 values: ');
+  writeln('Enter 10 values: ');
   for i := 1 to N do
-    begin
-      write ('Element №', i,': ');
-      readln (A[i]);
-    end;
+  begin
+    write('Element №', i, ': ');
+    readln(A[i]);
+  end;
 
-  for i:=1 to N - 1 do
-    begin
-      min := i;
+  for i := 1 to N - 1 do
+  begin
+    min := i;
 
-      for j := i + 1 to N do
-        if A[j] < A[min] then min := j;
+    for j := i + 1 to N do
+      if A[j] < A[min] then
+        min := j;
 
-      tmp := A[i];
-      A[i] := A[min];
-      A[min] := tmp;
-    end;
+    tmp := A[i];
+    A[i] := A[min];
+    A[min] := tmp;
+  end;
 
   for i := 1 to N do
-    begin
-      writeln('Element № ', i, ' Value: ', A[i]);
-    end;
+  begin
+    writeln('Element № ', i, ' Value: ', A[i]);
+  end;
 
   readln;
+
 end.
